4c20d5d102f9aef09436ddbbf35b7e6d9b206fd0
31201718LEO <gabrielav362@gmail.com>
2024-06-05 21:35:17 -0300

    Aulsa 05/06/2024 de Javascript

---
+++ 10.entrada_user_html.2.js
@@ -0,0 +1,12 @@
+// Entrada do usuário utilizando Pompt no Navegador 
+
+var num1 = prompt ("Digite um número: ");
+var num2 = prompt (" Digite outro número: ");
+
+let soma = Number(num1) + Number(num2);
+
+alert( "O resultado da soma é : ")
+
+document.write("<h2 align=center> O resultado da soma é " + soma + "</h2>");
+
+
